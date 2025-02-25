2944d88ead09535e9708186f55b34208852054c8
Kasymzhomart <kasymzhom1978@gmail.com>
2025-02-25 23:09:28 +0500

    lb5

--- lab5/3.py
+++ lab5/3.py
@@ -1,6 +1,6 @@
 import re
 
-with open('row.txt','r') as file:
+with open(r'C:\Users\Касымжомарт\Desktop\pp\lab5\row.txt', 'r', encoding='utf-8') as file:
     text = file.read()
 
 pattern = '[a-z]_[a-z]'
